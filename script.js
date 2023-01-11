2579e28c2270d4642798269c524eaafb460b9860
styled <smyvensestime@gmail.com>
2023-01-10 22:16:04 -0500

    removed async/await and improve readability

--- script.js
+++ script.js
@@ -1,87 +1,99 @@
 const cards = document.querySelectorAll(".card")
-const scoreElement = document.getElementById("score")
-const messageElement = document.getElementById("message")
+const scoreElement = document.getElementById('score')
+const messageElement = document.getElementById('message')
 
-const highlightList = [] // the order of which the cards will highlight a card
-let isListening = false // if the program will respond to the user clicks
-let userClickIndex = 0
+let pattern = []  // the order of which the cards will highlight a card
+let isListening = false  // if the program will respond to the user clicks
+let userProgress = 0  // how much of the pattern the user has clicked
 
 let score = 0
 
 for (const card of cards) {
-	card.addEventListener("click", function (event) {
-		if (isListening) {
-			const cardId = event.target.getAttribute("id")
-			handleCardClick(cardId)
-		}
-	})
+    card.addEventListener("click", function (event) {
+        if (isListening) {
+            const cardId = event.target.getAttribute("id")
+            handleCardClick(cardId)
+        }
+    })
 }
 
 function getRandomNumber() {
-    // picks a number between 1 and 10
-	return Math.ceil(Math.random() * 10)
+    // random number between 1 and 10
+    // Important: cannot go above the number of cards that you have from the html document
+    return Math.ceil(Math.random() * 10)
 }
 
-function sleep(milleseconds) {
-	return new Promise(resolve => setTimeout(resolve, milleseconds))
+function highlightCard(card) {
+    document.getElementById(card).classList.add("highlight")
+    //  automatically remove the highlight from the card after a delay of 1s
+    setTimeout(unHighlightCard, 1000, card);
 }
 
-async function highlightCards() {
-	isListening = false
+function unHighlightCard(card) {
+    document.getElementById(card).classList.remove("highlight")
+}
+
+function showPattern() {
 	messageElement.textContent = "Watch the pattern!"
+    let delay = 1000 // javascript uses milliseconds (1000ms = 1s)
 
-	// loop through the random numbers that the computer has picked
-	for (i in highlightList) {
-		const cardElement = document.getElementById(highlightList[i])
+    // loop through each card from the pattern
+    for (const card of pattern)  {
+        // delay by n seconds, highlight the card after that delay
+        setTimeout(highlightCard, delay, card);
 
-        await sleep(250)
-
-        // add the highlight
-        cardElement.classList.add("highlight")
-        
-        await sleep(250)
-
-		// remove the highlight
-		cardElement.classList.remove("highlight")
-	}
-
-	isListening = true
+        // increment the delay by 1 second to give a buffer between each cards showing
+        delay += 1000
+    }
 	messageElement.textContent = "Your turn!"
 }
 
 function handleCardClick(cardId) {
-	// see if what the user's history matches up with the computers history
-	// if they do, increment the click index
-	// if not, reset the click index and score back to 0
-	if (highlightList[userClickIndex] == cardId) {
-		userClickIndex++
-	} else {
-		score = 0
-		userClickIndex = 0
-	}
+    // check if the user clicked on the correct card
+    if (pattern[userProgress] == cardId) {
+        userProgress++
+    } else {
+        // if they did not, reset the game
+        resetGame()
+    }
+    
+    // check to see if the user clicked everything from the pattern
+    if (userProgress > 0 && userProgress == pattern.length) {
+        // update the score
+        score++
+        // add a new card to the pattern
+        pattern.push(getRandomNumber())
+        // reset the click index
+        userProgress = 0
+        // show the new cards
+        showPattern()
+    }
 
-	// if the user clicked everything from the history
-	if (userClickIndex == highlightList.length) {
-		// update the score
-		score++
-		// add a new item to the history
-		highlightList.push(getRandomNumber())
-		// reset the click index
-		userClickIndex = 0
-		// show the new cards
-		highlightCards()
-	}
-
-	// modify the score on the screen so the user could see
-	scoreElement.innerText = "Score: " + score
+    // modify the score on the screen so the user could see
+    scoreElement.innerText = 'Score: ' + score
 }
 
-function start() {
-	// pick a card to highlight
-	highlightList.push(getRandomNumber())
+function resetGame() {
+    isListening = false
 
-	// show the highlighting so the user can view
-	highlightCards()
+    score = 0
+    userProgress = 0
+    pattern = []
+
+    isListening = true
+    startGame()
 }
 
-start()
+function startGame() {
+    // pick a card to highlight
+    pattern.push(getRandomNumber())
+
+    // show the highlighting so the user can view
+    showPattern()
+
+    // and listen for what the user is clicking on
+    isListening = true
+}
+
+
+startGame()
